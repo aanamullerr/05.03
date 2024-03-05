c66a36b2b407d662132831f5a92cafd5c1767566
aanamullerr <126471463+aanamullerr@users.noreply.github.com>
2024-03-05 11:29:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// comando para exibir mensagem
+console.log("O GREMIO VAI SAIR CAMPEÃO E O INTER NÃO")
+
+// exercicio de calculos
+
+const num1 = 20;
+const num2 = 654;
+
+// soma
+let soma = num1 + num2;
+console.log("O valor da soma é " + soma + " - dale gremio");
+
+// subtração
+let subtração = num1 - num2;
+console.log("O valor da subtração é " + subtração + " - dale gremio");
+
+// multiplicação
+let multiplicação = num1 * num2;
+console.log("O valor da multiplicação é " + multiplicação + " - dale gremio");
+
+// divisão
+let divisão = num1 / num2;
+console.log("O valor da divisão é " + divisão + " - dale gremio");
